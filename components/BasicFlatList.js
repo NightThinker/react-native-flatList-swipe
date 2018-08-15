56e88c8e12a6b5ab5a27bf7bf49d743cfb68bea0
Jiraphon <jiraphon@homedottech.com>
2018-08-15 10:47:59 +0700

    image

--- components/BasicFlatList.js
+++ components/BasicFlatList.js
@@ -2,21 +2,46 @@
 Mr Nguyen Duc Hoang
 https://www.youtube.com/c/nguyenduchoang
 Email: sunlight4d@gmail.com
-FlatList Component
+FlatList Component with Images
 */
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
-import flatListData from '';
+import { AppRegistry, FlatList, StyleSheet, Text, View, Image } from 'react-native';
+import flatListData from '../data/flatListData';
 
 class FlatListItem extends Component {
     render() {          
-        return (
-          <View style={{
+        return (        
+            <View style={{
                 flex: 1,
-                backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen': 'tomato'                
-          }}>            
-              <Text style={styles.flatListItem}>{this.props.item.name}</Text>
-              <Text style={styles.flatListItem}>{this.props.item.foodDescription}</Text>
+                flexDirection:'column',                                
+            }}>            
+                <View style={{
+                        flex: 1,
+                        flexDirection:'row',
+                        // backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen': 'tomato'                
+                        backgroundColor: 'mediumseagreen'
+                }}>            
+                    <Image 
+                        source={{uri: this.props.item.imageUrl}}
+                        style={{width: 100, height: 100, margin: 5}}
+                    >
+
+                    </Image>
+                    <View style={{
+                            flex: 1,
+                            flexDirection:'column',   
+                            height: 100                 
+                        }}>            
+                            <Text style={styles.flatListItem}>{this.props.item.name}</Text>
+                            <Text style={styles.flatListItem}>{this.props.item.foodDescription}</Text>
+                    </View>              
+                </View>
+                <View style={{
+                    height: 1,
+                    backgroundColor:'white'                            
+                }}>
+            
+                </View>
           </View>
         );
     }
